e7daa19fb292b95a9b2423049f251d2d306eee68
marzookh <farook@ualberta.ca>
2022-10-13 22:18:18 -0600

    update

--- httpclient.py
+++ httpclient.py
@@ -18,6 +18,7 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+from ast import arg
 import sys
 import socket
 import re
@@ -33,21 +34,35 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self,url):
+        parsed_url = urllib.parse.urlparse(url)
+        host = parsed_url.hostname
+        path = parsed_url.path
+        if path == '':
+            path = '/'
+        port = parsed_url.port
+        if port is None:
+            port = 80
+        query = parsed_url.query
+        return host, path, port, query
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
-        return None
+        return self.socket
 
     def get_code(self, data):
-        return None
+        return int(data.split()[1])
 
     def get_headers(self,data):
-        return None
+        split_data = data.split('\r\n\r\n')
+        header = split_data[0]
+        return header
 
     def get_body(self, data):
-        return None
+        split_data = data.split('\r\n\r\n')
+        body = split_data[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,13 +83,61 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
+        code = 0
         body = ""
+        host, path, port, query = self.get_host_port(url)
+
+        if query:
+            request_body = f"GET {path}?{query} HTTP/1.1\r\nHost:{host}\r\nAccept: */*\r\nConnection: close\r\n\r\n"
+
+        request_body = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nAccept: */*\r\nConnection: close\r\n\r\n"
+
+        self.connect(host,port)
+        self.sendall(request_body)
+
+        new_data = self.recvall(self.socket)
+        code = self.get_code(new_data)
+        body = self.get_body(new_data)
+        print(body)
+        # print response
+        #print(new_data)
+
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+        host, path, port, query = self.get_host_port(url)
+
+        # get the content length
+        args_content = ''
+        if args is not None:
+            for args_key in args:
+                args_content = args_content + args_key
+                argument = args[args_key]
+                args_content += '=' + argument + '&'
+            
+            args_content = args_content[:-1]
+            args_content_length = len(args_content)
+            request_body = f"POST {path} HTTP/1.1\r\nHost:{host}\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-length:{args_content_length}\r\nConnection: close\r\n\r\n{args_content}"
+            #request_body += args_content_length + '\r\n\r\n' + args_content
+        elif query is not None:
+            request_body = f"POST {path} HTTP/1.1\r\nHost:{host}\r\nContent-Type: application/x-www-form-urlencoded\r\n{query}Content-length:{len(query)}\r\nConnection: close\r\n\r\n"
+        else:
+            request_body = f"POST {path} HTTP/1.1\r\nHost:{host}\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-length:0\r\nConnection: close\r\n\r\n"
+
+        self.connect(host,port)
+        self.sendall(request_body)
+
+        new_data = self.recvall(self.socket)
+        code = self.get_code(new_data)
+        body = self.get_body(new_data)
+        print(body)
+        # print response
+        #print(new_data)
+        
+        self.close()
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
